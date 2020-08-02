2407d8e5883ff99983af4f4653ae81b0d530b403
Yaroslav Serhieiev <noomorph@gmail.com>
2020-08-02 16:40:16 +0300

    wip: rewrite local-cli/test.js

---
+++ detox/local-cli/utils/misc.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const environment = require('../../src/utils/environment');
+
+function clearDeviceRegistryLockFile(platform) {
+  const lockFilePath = platform === 'ios'
+    ? environment.getDeviceLockFilePathIOS()
+    : environment.getDeviceLockFilePathAndroid();
+
+  fs.ensureFileSync(lockFilePath);
+  fs.writeFileSync(lockFilePath, '[]');
+}
+
+function getPlatformSpecificString(platform) {
+  switch (platform) {
+    case 'ios': return ':android:';
+    case 'android': return ':ios:';
+    default: return undefined;
+  }
+}
+
+function printEnvironmentVariables(envObject) {
+  return Object.entries(envObject).reduce((cli, [key, value]) => {
+    if (value == null || value === '') {
+      return cli;
+    }
+
+    return `${cli}${key}=${JSON.stringify(value)} `;
+  }, '');
+}
+
+module.exports = {
+  clearDeviceRegistryLockFile,
+  getPlatformSpecificString,
+  printEnvironmentVariables,
+};
